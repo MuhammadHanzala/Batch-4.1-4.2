ba25a6f32ae7cd22cdb1fb84bd7314b5ebfc734a
MuhammadHanzala <mhanla989@gmail.com>
2017-09-13 05:36:22 -0700

    ui updated

--- Dua App/home.js
+++ Dua App/home.js
@@ -1,12 +1,10 @@
 var user = JSON.parse(localStorage.getItem('currentUser'));
-var nameRow = document.getElementById('nameRow');
-var emailRowail = document.getElementById('emailRow');
 var sender = document.getElementById('sender');
 var comment = document.getElementById('commentBox');
+var welcomeTag = document.getElementById('welcome');
 var database = firebase.database().ref();
 
-nameRow.innerHTML = user.name;
-emailRow.innerHTML = user.email;
+welcomeTag.innerHTML = "Welcome " + user.name;
 
 
 function submit(){
