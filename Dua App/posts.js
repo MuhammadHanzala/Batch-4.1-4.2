20a504fc45cb8e8853e7885abfdad8895c6eb897
MuhammadHanzala <mhanzala989@gmail.com>
2017-09-14 09:54:44 -0700

    rendering comments

--- Dua App/posts.js
+++ Dua App/posts.js
@@ -9,35 +9,51 @@ database.child("posts").on("child_added", function(snapshot){
 })
 database.child("comments").on("child_added", function(snapshot){
     var obj = snapshot.val();
-    // renderComment(obj);
+    renderComment(obj);
 })
 
 function render(dua){
+    var mainDiv = document.createElement("DIV");
+    mainDiv.setAttribute("id", dua.id);
+    mainDiv.setAttribute("class", "card dua");
+    
     var div = document.createElement("DIV");
-    div.setAttribute("id", dua.id);
+    div.setAttribute("class", "card-body");
 
     var span = document.createElement("SPAN");
-    var sender = document.createTextNode("Name: " + dua.sender +  ' ');
-    var duaText = document.createTextNode("Dua: " + dua.dua);
+    var senderTag = document.createElement("H4");
+    senderTag.setAttribute("class", "card-title");
+    var sender = document.createTextNode(dua.sender);
+    senderTag.appendChild(sender);
 
-    span.appendChild(sender);
-    span.appendChild(duaText);
+    var duaTag = document.createElement("H6");
+    duaTag.setAttribute("class", "card-text");    
+    var duaText = document.createTextNode(dua.dua);
+    duaTag.appendChild(duaText);
+
+    span.appendChild(senderTag);
+    span.appendChild(duaTag);
     div.appendChild(span);
 
     var commentBox = document.createElement("INPUT");
+    commentBox.setAttribute("class", "form-control");
     commentBox.setAttribute("id", "comment" + dua.id);
+    
     var btn = document.createElement("BUTTON");
+    btn.setAttribute("class", "btn btn-primary");
     var btnText = document.createTextNode("Comment");
     btn.onclick = function(){
         submitComment(dua.id);
     }
+
     div.appendChild(commentBox);
     div.appendChild(btn);
 
     btn.appendChild(btnText);
-
-
-    posts.appendChild(div);
+    var commentsDiv = document.createElement("DIV");
+    mainDiv.appendChild(commentsDiv);
+    mainDiv.appendChild(div);
+    posts.appendChild(mainDiv);
 }
 function submitComment(duaId){
     var commentInput = document.getElementById("comment" + duaId);
@@ -48,4 +64,31 @@ function submitComment(duaId){
     }
     database.child("comments").push(commentObj);
     commentInput.value = '';
+}
+function renderComment(comment){
+    var duaDiv = document.getElementById(comment.duaId);
+    var commentsDiv = duaDiv.lastElementChild;
+    
+    var card = document.createElement("DIV");
+    card.setAttribute("class", "card");
+    
+    var cardBody = document.createElement("DIV");
+    cardBody.setAttribute("class", "card-body");
+
+    var senderTag = document.createElement("H5");
+    senderTag.setAttribute("class", "card-title");
+    var sender = document.createTextNode(comment.sender);
+    senderTag.appendChild(sender);
+
+    var commentTag = document.createElement("H6");
+    commentTag.setAttribute("class", "card-text");
+    var commentText = document.createTextNode(comment.comment);
+    commentTag.appendChild(commentText);
+
+    cardBody.appendChild(senderTag);
+    cardBody.appendChild(commentTag);
+
+    card.appendChild(cardBody);
+
+    commentsDiv.appendChild(card);
 }
